5e003d03676132558bdd6cfabb604c10c0a81a96
Sanduntharaka00t <160314515+Sanduntharaka00t@users.noreply.github.com>
2024-07-06 13:56:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,14 +8,14 @@ if (fs.existsSync('config.env')) {
 }
 
 const API = "https://api-aswin-sparky.koyeb.app";
-const HANDLERS = process.env.HANDLER || "^";
-const SESSION_ID = process.env.SESSION_ID || "";
-const SUDO = process.env.SUDO || '917012984396';
+const HANDLERS = process.env.HANDLER || "@";
+const SESSION_ID = process.env.SESSION_ID || "A-S-W-I-N-S-P-A-R-K-Y:e58884428734f415ac41e4a721fe84b3";
+const SUDO = process.env.SUDO || '94728756898';
 const AUTO_STATUS_VIEW = process.env.AUTO_STATUS_VIEW || "true";
-const ALWAYS_ONLINE = process.env.ALWAYS_ONLINE || "true";
+const ALWAYS_ONLINE = process.env.ALWAYS_ONLINE || "false";
 const BOT_INFO = process.env.BOT_INFO || "X-BOT-MD;ASWIN SPARKY;https://i.imgur.com/r3GZeiX.jpeg";
-const URL = process.env.URL || "https://www.instagram.com/imspxxky";
-const AUDIO_DATA = process.env.AUDIO_DATA || "X BOT MD;ASWIN SPARKY;https://i.imgur.com/fVCRCYG.jpeg";
+const URL = process.env.URL || "https://whatsapp.com/channel/0029VaglXx3LikgF18gU651T";
+const AUDIO_DATA = process.env.AUDIO_DATA || "https://telegra.ph/file/0a776b2a20271ec6d93b4.jpg";
 const STICKER_DATA = process.env.STICKER_DATA || "X BOT MD;ASWIN SPARKY";
 const WORK_TYPE = process.env.WORK_TYPE || 'public';
 const DATABASE_URL = process.env.DATABASE_URL || "./lib/database.db";
